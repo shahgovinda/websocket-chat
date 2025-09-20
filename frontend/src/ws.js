2f07bbe8019253849cc847d1dd6504d248ea9842
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-09-20 09:16:12 +0000

    update

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    return io('https://potential-space-carnival-7j9gx99w9v5frpp-4600.app.github.dev');
+    return io('https://websocket-chat-teal.vercel.app');
 }
