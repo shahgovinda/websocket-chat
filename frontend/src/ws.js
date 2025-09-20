a2a63300221aa81135fc0da097b22cf4dfa6d756
Govinda J Shah <96227277+shahgovinda@users.noreply.github.com>
2025-09-20 08:59:37 +0000

    Update

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    return io('http://localhost:4600');
+    return io('https://potential-space-carnival-7j9gx99w9v5frpp-4600.app.github.dev');
 }
